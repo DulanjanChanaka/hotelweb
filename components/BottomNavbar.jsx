9419ecddc4cdd2a0ce907eb303dff860744ecefd
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-10-26 00:45:40 +0530

    bottom nav bar position change

--- components/BottomNavbar.jsx
+++ components/BottomNavbar.jsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 
 function BottomNavbar() {
   return (
-    <div className={`lg:hidden sticky bottom-0   left-0 w-full py-5 z-50 shadow-xl bg-red-700 text-white`}  >
+    <div className='lg:hidden sticky bottom-0   left-0 w-full py-5 z-50 shadow-xl bg-red-700 text-white'  >
       <div className='flex flex-row justify-around'>
 
         <Link href='/'><div ><HomeOutlinedIcon /></div></Link>
